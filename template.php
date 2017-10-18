7f4108720bf1f4a5887ca69a402a43d17cc7f899
D. Fellipe de Palma <dfellipe7@gmail.com>
2017-10-18 21:57:52 -0200

    Correção CRUD Usuário

--- template.php
+++ template.php
@@ -1,23 +1,29 @@
+<!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="utf-8">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+		<link rel="stylesheet" href="css/estilos.css">
+
+		<title>Login | Hippo</title>
+	</head>
 	<body>
-		<br><br>
-		<center>
-			<br><br><strong>Login</strong><br><br>
-			<form method="POST">
+
+			<form method="POST" action="">
+				<img class="logo" src="img/logo.jpg">
+					<h1>Login</h1>
 				<?php
 				if(isset($msg)){
-					echo "<br><b>$msg</b><br><br>";					
+					echo "<br><b>$msg</b><br><br>";
 				}
 				?>
-				<input type="text" name="login">
-				<br><br>
-				<input type="password" name="senha">
-				<br><br>
-				<input type="submit" value="Login" 
-				name="btnLogin">
-			
+				<label>Login</label>
+				<input type="text" name="login" >
+				<label>Senha</label>
+				<input type="password" name="senha" >
+				<input type="submit" value="Login" name="btnLogin" class="btn">
 			</form>
-		
+
 		</center>
 	</body>
 </html>
